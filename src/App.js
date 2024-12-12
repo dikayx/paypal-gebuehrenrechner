6d527011dbe2eccf36163230a5fbba15576c462f
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2024-12-12 08:05:39 +0100

    Updated styling
    Fixed a bug where the badge won't stick to the bottom. Also transformed the fork me badge to a neat little sphere on mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FaGithub } from "react-icons/fa"; // Import GitHub icon
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
 import "./App.css";
 import Navbar from "./Navbar";
@@ -9,9 +10,9 @@ import { Container, Box, Typography } from "@mui/material";
 
 function App() {
     return (
-        <Router basename="/paypal-fee-calculator">
+        <Router basename='/paypal-fee-calculator'>
             <Navbar />
-            <Container maxWidth="lg" sx={{ mt: 4 }}>
+            <Container maxWidth='lg' sx={{ mt: 4 }}>
                 <Box
                     sx={{
                         p: 3,
@@ -21,7 +22,7 @@ function App() {
                     }}
                 >
                     <Typography
-                        variant="h4"
+                        variant='h4'
                         gutterBottom
                         sx={{ textAlign: "center", mb: 4 }}
                     >
@@ -29,19 +30,32 @@ function App() {
                     </Typography>
 
                     <Routes>
-                        <Route path="/" element={<PayPalFeeCalculator />} />
-                        <Route path="/fees-overview" element={<FeesOverview />} />
+                        <Route path='/' element={<PayPalFeeCalculator />} />
+                        <Route
+                            path='/fees-overview'
+                            element={<FeesOverview />}
+                        />
                     </Routes>
                 </Box>
             </Container>
 
+            {/* GitHub Fork Ribbon (Desktop view) */}
             <a
-                className="github-fork-ribbon right-bottom"
-                href="https://github.com/dikayx/paypal-fee-calculator"
-                data-ribbon="Fork me on GitHub"
-                title="Fork me on GitHub"
+                className='github-fork-ribbon right-bottom'
+                href='https://github.com/dikayx/paypal-fee-calculator'
+                data-ribbon='Fork me on GitHub'
+                title='Fork me on GitHub'
             >
                 Fork me on GitHub
+            </a>
+
+            {/* GitHub Circle Badge (Mobile view) */}
+            <a
+                className='github-circle right-bottom-mobile'
+                href='https://github.com/dikayx/paypal-fee-calculator'
+                title='Fork me on GitHub'
+            >
+                <FaGithub className='github-icon' />
             </a>
         </Router>
     );
