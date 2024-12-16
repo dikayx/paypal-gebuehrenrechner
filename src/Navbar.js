08b7a9e27e670177d0051b927de284ae71ce7c5f
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2024-12-16 14:34:39 +0100

    Added mobile navigation drawer to Navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     AppBar,
     Toolbar,
@@ -6,17 +6,44 @@ import {
     Container,
     Button,
     Box,
+    IconButton,
+    Drawer,
+    List,
+    ListItem,
+    ListItemText,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
 
 function Navbar() {
+    const [mobileOpen, setMobileOpen] = useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
+
+    const drawerContent = (
+        <Box sx={{ width: 250 }} onClick={handleDrawerToggle}>
+            <List>
+                <ListItem button component={Link} to='/fees-overview'>
+                    <ListItemText primary='Gebührenübersicht' />
+                </ListItem>
+            </List>
+        </Box>
+    );
+
     return (
         <AppBar position='static'>
             <Toolbar>
                 <Container
                     maxWidth='lg'
-                    sx={{ display: "flex", justifyContent: "space-between" }}
+                    sx={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                    }}
                 >
+                    {/* Title */}
                     <Typography
                         variant='h6'
                         component={Link}
@@ -28,7 +55,9 @@ function Navbar() {
                     >
                         PayPal Gebührenrechner
                     </Typography>
-                    <Box>
+
+                    {/* Desktop View */}
+                    <Box sx={{ display: { xs: "none", md: "block" } }}>
                         <Button
                             color='inherit'
                             component={Link}
@@ -38,8 +67,28 @@ function Navbar() {
                             Gebührenübersicht
                         </Button>
                     </Box>
+
+                    {/* Mobile Hamburger Menu */}
+                    <Box sx={{ display: { xs: "block", md: "none" } }}>
+                        <IconButton
+                            color='inherit'
+                            edge='start'
+                            onClick={handleDrawerToggle}
+                        >
+                            <FiMenu size={24} /> {/* Hamburger icon */}
+                        </IconButton>
+                    </Box>
                 </Container>
             </Toolbar>
+
+            {/* Drawer for Mobile View */}
+            <Drawer
+                anchor='right'
+                open={mobileOpen}
+                onClose={handleDrawerToggle}
+            >
+                {drawerContent}
+            </Drawer>
         </AppBar>
     );
 }
